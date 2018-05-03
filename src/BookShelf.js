82733004420bd1f426b4474655f1113e664501aa
stdAlexTikhonov <stdAlexTikhonov@hotmail.com>
2018-05-03 22:18:43 +0300

    fix: set value on select tag

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -32,7 +32,7 @@ class BookShelf extends Component {
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url("${book.cover}")` }}></div>
                             <div className="book-shelf-changer">
-                              <select onChange={this.handleChange} data-id={book.id}>
+                              <select onChange={this.handleChange} data-id={book.id} value={book.status}>
                                 <option value="none" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
--- src/Search.js
+++ src/Search.js
@@ -57,7 +57,7 @@ class Search extends Component {
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url("${book.cover}")` }}></div>
                             <div className="book-shelf-changer">
-                              <select onChange={this.handleChangeStatus} data-id={book.id}>
+                              <select onChange={this.handleChangeStatus} data-id={book.id} value={book.status}>
                                 <option value="none" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
