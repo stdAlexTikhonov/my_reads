c5bf2c2190a58337872c31d3696b7a621306b07b
stdAlexTikhonov <stdAlexTikhonov@hotmail.com>
2018-05-04 11:46:43 +0300

    fix: ListBooks converted to functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,35 +3,32 @@ import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf';
 // import BookShelf from './BookShelf'
 
-class ListBooks extends Component {
+function ListBooks(props) {
 
+    const { books, onChangeBookStatus } = props;
 
-    render() {
+    const booksCR = books.filter(book => book.shelf === 'currentlyReading'),
+          booksWR = books.filter(book => book.shelf === 'wantToRead'),
+          booksR = books.filter(book => book.shelf === 'read');
 
-        const { books, onChangeBookStatus } = this.props;
-
-        const booksCR = books.filter(book => book.shelf === 'currentlyReading'),
-              booksWR = books.filter(book => book.shelf === 'wantToRead'),
-              booksR = books.filter(book => book.shelf === 'read');
-
-        return (
-            <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <BookShelf key={0} books={booksCR} onChange={onChangeBookStatus}/>
-                <BookShelf key={1} books={booksWR} onChange={onChangeBookStatus}/>
-                <BookShelf key={2} books={booksR} onChange={onChangeBookStatus}/>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to='/search'>Add a book</Link>
-            </div>
+    return (
+        <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf key={0} books={booksCR} onChange={onChangeBookStatus}/>
+            <BookShelf key={1} books={booksWR} onChange={onChangeBookStatus}/>
+            <BookShelf key={2} books={booksR} onChange={onChangeBookStatus}/>
           </div>
-        )
-    }
+        </div>
+        <div className="open-search">
+          <Link to='/search'>Add a book</Link>
+        </div>
+      </div>
+    )
+
 }
 
 
