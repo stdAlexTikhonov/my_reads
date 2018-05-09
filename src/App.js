cdd110a054a8bc3d2515ac7ff34311fe6c917e0b
stdAlexTikhonov <stdAlexTikhonov@hotmail.com>
2018-05-10 02:13:25 +0300

    feat: search page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+function transform(book) {
+  return {
+    id: book.id,
+    status: book.shelf,
+    title: book.title ? book.title : 'Unknown',
+    author: book.authors ? book.authors[0] : 'Unknown',
+    cover: book.imageLinks ? book.imageLinks.thumbnail : null
+  }
+}
+
 class BooksApp extends React.Component {
   state = {
     /**
@@ -10,13 +20,16 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+    query: '',
+    searched: [],
     books: [],
     showSearchPage: false
   }
 
   onChangeStatus = (e) => {
     let shelf = e.target.value,
-        id = +e.target.getAttribute('dataid');
+        id = e.target.getAttribute('dataid');
+
 
     this.setState((prev)=>({
       books: prev.books.map(book => {
@@ -26,22 +39,37 @@ class BooksApp extends React.Component {
     }))
 
     BooksAPI.update({id: id}, shelf);
+  }
 
 
+  onChangeQuery = (e) => {
+    let val = e.target.value;
+    this.setState({
+      query: val
+    });
+
+    BooksAPI.search(val !== '' ? val : ' ').then(books => {
+      console.log(books);
+      if (books.error) {
+        books = [];
+      }
+
+      this.setState({
+        searched: books.map(transform)
+      })
+      
+
+    });
   }
 
   componentDidMount() {
     BooksAPI.getAll().then(books => {
-      books = books.map(book => {
-        return {
-          id: book.id,
-          status: book.shelf,
-          title: book.title,
-          author: book.authors[0],
-          cover: book.imageLinks.thumbnail
-        }
-      });
+
+      this.setState({
+        books: books.map(transform)
+      })
     });
+
   }
 
   render() {
@@ -65,12 +93,36 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text" onChange={this.onChangeQuery} placeholder="Search by title or author" value={this.state.query}/>
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+                { this.state.searched.length ?  (<ol className="books-grid">
+                  {
+                            this.state.searched.map((book,i) => (
+                              <li key={'cr' + i}>
+                              <div className="book">
+                                <div className="book-top">
+                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.cover}")` }}></div>
+                                  <div className="book-shelf-changer">
+                                    <select onChange={this.onChangeStatus} value={book.status} dataid={book.id}>
+                                      <option disabled>Move to...</option>
+                                      <option value="currentlyReading">Currently Reading</option>
+                                      <option value="wantToRead">Want to Read</option>
+                                      <option value="read">Read</option>
+                                      <option value="none">None</option>
+                                    </select>
+                                  </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.author}</div>
+                              </div>
+                            </li>
+                            ))
+                      }
+                  </ol>) : (<div style={{color: 'red', fontStyle: 'italic'}} >Books not found</div>) }
+             
             </div>
           </div>
         ) : (
@@ -91,7 +143,7 @@ class BooksApp extends React.Component {
                             <div className="book-top">
                               <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.cover}")` }}></div>
                               <div className="book-shelf-changer">
-                                <select onChange={this.onChangeStatus} value={book.status} dataid={book.id}>
+                                <select onChange={(e) => { this.onChangeStatus(e) }} value={book.status} dataid={book.id}>
                                   <option disabled>Move to...</option>
                                   <option value="currentlyReading">Currently Reading</option>
                                   <option value="wantToRead">Want to Read</option>
